ff1fa6c5ea3d3b15018fb9ed3e004a4e19063066
Tomt-ech <adewaletomiwa77@gmail.com>
2022-09-08 07:34:05 -0700

    Task 5 fixed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
  * this program print with proper grammar, but the outcome is a piece of art, 
  * Return: 0
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
