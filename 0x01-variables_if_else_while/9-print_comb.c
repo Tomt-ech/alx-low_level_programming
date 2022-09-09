078f45809e96d497e6daf5f06cc2405d0730af60
Tomt-ech <adewaletomiwa77@gmail.com>
2022-09-09 12:32:33 -0700

    Task 9 done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Discription: project details
+ * Return: 0
+ */
+int main(void)
+{
+		int n = '0';
+
+		while (n <= '9')
+			{
+				putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');																		}
+			n++;
+			}
+		putchar('\n');
+
+		return (0);
+}
