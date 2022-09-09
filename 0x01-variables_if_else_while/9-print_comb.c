359564129253d9b56db07acdb426c9d7e38adff9
Tomt-ech <adewaletomiwa77@gmail.com>
2022-09-09 12:41:27 -0700

    Task 9 done

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,10 +10,10 @@ int main(void)
 {
 		int n;
 
-		for (n = 48; n < 57; c++)
+		for (n = '48'; n < '57'; c++)
 			{
 				putchar(n);
-		if (n != 57)
+		if (n != '57')
 		{
 			putchar(',');
 			putchar(' ');
