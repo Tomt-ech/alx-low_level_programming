b1ae62adc0d928b5c126420d201ededdfa0701c2
Tomt-ech <adewaletomiwa77@gmail.com>
2022-09-09 15:41:54 -0700

    Hot fixed

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,14 +11,14 @@ int main(void)
 {
 	char alpha = 'a';
 
-		while (alpha <= 'z')
-			{
-				if ((alpha != 'q') && (alpha != 'e'))
-					{
-						putchar(alpha);
-					}
+	while (alpha <= 'z')
+		{
+			if ((alpha != 'q') && (alpha != 'e'))
+				{
+					putchar(alpha);
+				}
 			alpha++;
-			}
+		}
 			putchar('\n');
 
 			return (0);
