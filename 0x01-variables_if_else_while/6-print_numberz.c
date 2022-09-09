e3912cf30d65d21a11dc07a5aac1a9fad57a1a70
Tomt-ech <adewaletomiwa77@gmail.com>
2022-09-09 11:12:47 -0700

    Task 6 done

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,8 +10,10 @@ int main(void)
 {
 	int num;
 
-		for (num = 0; num < 10; num++)
-			putchar((num % 10) + '0');
+		for (num = 48; num < 58; num++)
+		{
+			putchar(num);
+		}
 			putchar('\n');
 		return (0);
 }
