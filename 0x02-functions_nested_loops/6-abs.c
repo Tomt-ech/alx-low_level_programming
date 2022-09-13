67e052ea51297d908b24c4d83deb346acbf7f56b
Tomt-ech <adewaletomiwa77@gmail.com>
2022-09-13 15:26:46 -0700

    error fixed

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,14 +3,16 @@
 /**
  * _abs -  function that computes the absolute value of an integer
  * @n: The character to be checked.
- * return: 0
+ * Return: 0
  */
 
 int _abs(int n)
 {
 
-	if (n >= '0')
+	if (n > 0 || n == 0)
+	{
 		return (n);
-	else
-		return(-n);
+	}
+	else 
+		return (n * -1);
 }
