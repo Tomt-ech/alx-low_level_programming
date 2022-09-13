b571dfe0abff896536759bf2d36ce5f0c3c383cb
Tomt-ech <adewaletomiwa77@gmail.com>
2022-09-12 23:26:48 -0700

    Task 0 done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point
+ *
+ * Description: The project detail
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('a');
+	_putchar('h');
+	_putchar('r');
+	_putchar('\n');
+
+	returun (0);
+}
