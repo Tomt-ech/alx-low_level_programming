122dd0f83dd7eeeb04e6fe9cc5794bae690ac2a7
Tomt-ech <adewaletomiwa77@gmail.com>
2022-09-12 23:42:50 -0700

    Task 1 done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point
+ *
+ * Description: The project details
+ * Return(0)
+ */
+
+void print_alphabet(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+		_putchar('alp');
+
+	_putchar('\n');
+}
