fb8a247a02a238ae69461f6b2cb73d0a86a5c472
Tomt-ech <adewaletomiwa77@gmail.com>
2022-09-21 23:56:46 -0700

    Error fixed

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * string_toupper - changes all lowercase letters of a string
- * to uppercase.
+ * string_toupper - changes all lowercase letters
+ * of a string to uppercase.
  * @str: string to modify
  *
  * Return: the resulting string
