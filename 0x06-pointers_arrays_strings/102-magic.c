6ab07f5a9291ea0b6b41adc25d7b5e7fd9ee5377
Tomt-ech <adewaletomiwa77@gmail.com>
2022-09-23 01:54:20 -0700

    Error fixed

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - entry point.
+ *
+ * Return: 0
+ */
 int main(void)
 {
 	int n;
@@ -19,5 +23,5 @@ int main(void)
 	 *(p + 5) = 98;
 	 /* ...so that this prints 98\n */
 	 printf("a[2] = %d\n", a[2]);
-	 return (0);
+	return (0);
 }
