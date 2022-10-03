c1430f0fd4252fa830b8528f1c4ae2d28eae58b8
Tomt-ech <adewaletomiwa77@gmail.com>
2022-10-03 00:15:41 -0700

    Almost done

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs -  function that computes the absolute value of an integer
+ * @n: The character to be checked.
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+
+	if (n > 0 || n == 0)
+	{
+		return (n);
+	}
+	else
+		return (n * -1);
+}
